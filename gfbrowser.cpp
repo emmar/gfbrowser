b8fb0e9d0f50fae593a5c1664f3833e99ec175bd
emmar <emma.ronney@gmail.com>
2015-06-09 16:00:21 +0000

    Update gfbrowser.cpp

--- gfbrowser.cpp
+++ gfbrowser.cpp
@@ -5,7 +5,7 @@
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
     QWebView view;
-    view.show();
+    view.showFullScreen();
     view.setUrl(QUrl("http://google.com"));
     return app.exec();
 }
