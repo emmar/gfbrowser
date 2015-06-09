ceae64f1630f888b8fce253a49ec7f41a46b0980
emmar <emma.ronney@gmail.com>
2015-06-09 16:17:27 +0000

    Update gfbrowser.cpp

--- gfbrowser.cpp
+++ gfbrowser.cpp
@@ -4,13 +4,14 @@
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
+    // Hide the cursor (it still exists).
     QApplication::setOverrideCursor(Qt::BlankCursor);
     QWebView view;
+    // Disable scrollbars.
     view.page()->mainFrame()->setScrollBarPolicy(Qt::Vertical, Qt::ScrollBarAlwaysOff);
     view.page()->mainFrame()->setScrollBarPolicy(Qt::Horizontal, Qt::ScrollBarAlwaysOff);
-    //view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    //view.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view.showFullScreen();
+    // Open a url.
     view.setUrl(QUrl("http://google.com"));
     return app.exec();
 }
