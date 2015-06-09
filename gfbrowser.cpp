50f85fc31b4432dc9dcaaa095f25070d9ef8b9eb
emmar <emma.ronney@gmail.com>
2015-06-09 16:15:15 +0000

    Update gfbrowser.cpp

--- gfbrowser.cpp
+++ gfbrowser.cpp
@@ -6,8 +6,8 @@ int main(int argc, char** argv) {
     QApplication app(argc, argv);
     QApplication::setOverrideCursor(Qt::BlankCursor);
     QWebView view;
-    view.page()->mainFrame()->setScrollBarPolicy( Qt::Vertical, Qt::ScrollBarAlwaysOff );
-    view.page()->mainFrame()->setScrollBarPolicy( Qt::Horizontal, Qt::ScrollBarAlwaysOff );
+    view.page().setScrollBarPolicy(Qt::Vertical, Qt::ScrollBarAlwaysOff);
+    view.page().setScrollBarPolicy(Qt::Horizontal, Qt::ScrollBarAlwaysOff);
     //view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     //view.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view.showFullScreen();
