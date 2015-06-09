9f4c780a710f370f2f4ac5e4bf809e39c8b4b93c
emmar <emma.ronney@gmail.com>
2015-06-09 16:06:56 +0000

    Update gfbrowser.cpp

--- gfbrowser.cpp
+++ gfbrowser.cpp
@@ -5,8 +5,10 @@
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
     QWebView view;
-    view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-	view.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view.page()->mainFrame()->setScrollBarPolicy( Qt::Vertical, Qt::ScrollBarAlwaysOff );
+    view.page()->mainFrame()->setScrollBarPolicy( Qt::Horizontal, Qt::ScrollBarAlwaysOff );
+    //view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    //view.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view.showFullScreen();
     view.setUrl(QUrl("http://google.com"));
     return app.exec();
