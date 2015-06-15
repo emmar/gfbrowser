147c78842f75919a4b4eead8454c1416285e5c7d
emmar <emma.ronney@gmail.com>
2015-06-15 19:21:25 +0000

    Update gfbrowser.cpp

--- gfbrowser.cpp
+++ gfbrowser.cpp
@@ -14,18 +14,21 @@ int main(int argc, char *argv[])
     else {url = "http://localhost";}
     // Hide the cursor (it still exists).
     //QApplication::setOverrideCursor(Qt::BlankCursor);
-
-QGraphicsScene scene;
-QGraphicsView view(&scene);
-QGraphicsWebView webView;
-scene.addItem(&webView);
-webView.load(QUrl("http://www.google.com"));
-view.showFullScreen();
-view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-view.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-webView.resize(app.desktop()->screenGeometry().size());	
-
-
+    QGraphicsScene scene;
+    QGraphicsView view(&scene);
+    QGraphicsWebView webView;
+    scene.addItem(&webView);
+    // Reccommended - http://blog.qt.io/blog/2010/05/17/qtwebkit-now-accelerates-css-animations-3d-transforms
+    view.setViewportUpdateMode(QGraphicsView::BoundingRectViewportUpdate);
+    view.setViewport(new QGLWidget);
+    // Load url.
+    webView.load(QUrl(url));
+    view.showFullScreen();
+    // Disable scrollbars.
+    view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    // bugfix for QGraphicsWebView
+    webView.resize(app.desktop()->screenGeometry().size());
 
     //QGraphicsScene scene;
     //QGraphicsView view(&scene);
