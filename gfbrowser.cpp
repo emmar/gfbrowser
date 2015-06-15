3aa9f8629dffcee027ba87f662e987b50f135299
emmar <emma.ronney@gmail.com>
2015-06-15 15:49:14 +0000

    Update gfbrowser.cpp

--- gfbrowser.cpp
+++ gfbrowser.cpp
@@ -6,7 +6,7 @@
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
     QString url;
-    QWebView view;
+    QGraphicsWebView view;
     // Read first application argument as url.
     if (1 < app.arguments().count()) {url = app.arguments().at(1);}
     else {url = "http://localhost";}
