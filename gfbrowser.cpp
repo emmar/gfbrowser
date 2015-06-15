76c475b8bbc654da812de64575aad6259d1d00de
emmar <emma.ronney@gmail.com>
2015-06-15 15:58:34 +0000

    Update gfbrowser.cpp

--- gfbrowser.cpp
+++ gfbrowser.cpp
@@ -1,12 +1,14 @@
 #include <QApplication>
-#include <QGraphicsWebView>
-#include <QWebFrame>
+#include <QGraphicsScene>
+#include <QGraphicsView>
 #include <QString>
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
     QString url;
-    QGraphicsWebView view;
+    QGraphicsScene scene;
+    QGraphicsView view(&scene);
+    QGraphicsWebView* webView  = new QGraphicsWebView();
     // Read first application argument as url.
     if (1 < app.arguments().count()) {url = app.arguments().at(1);}
     else {url = "http://localhost";}
@@ -15,8 +17,7 @@ int main(int argc, char** argv) {
     // Disable scrollbars.
     view.page()->mainFrame()->setScrollBarPolicy(Qt::Vertical, Qt::ScrollBarAlwaysOff);
     view.page()->mainFrame()->setScrollBarPolicy(Qt::Horizontal, Qt::ScrollBarAlwaysOff);
-    // Set full screen.
-    view.showFullScreen();
+    
     // Open a url.
     view.setUrl(QUrl(url));
     return app.exec();
