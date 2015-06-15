905ae9aa7a7fe199c6d0373a63d4a8c5863c92ef
emmar <emma.ronney@gmail.com>
2015-06-15 16:17:34 +0000

    Update gfbrowser.cpp

--- gfbrowser.cpp
+++ gfbrowser.cpp
@@ -16,7 +16,7 @@ int main(int argc, char** argv) {
     view.page()->mainFrame()->setScrollBarPolicy(Qt::Vertical, Qt::ScrollBarAlwaysOff);
     view.page()->mainFrame()->setScrollBarPolicy(Qt::Horizontal, Qt::ScrollBarAlwaysOff);
     // Set full screen.
-    view.showFullScreen();
+    //view.showFullScreen();
     // Open a url.
     view.setUrl(QUrl(url));
     return app.exec();
