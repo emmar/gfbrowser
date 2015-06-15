80623c6d9cb215e42390211099cc073d80ca7f7d
emmar <emma.ronney@gmail.com>
2015-06-15 16:31:40 +0000

    Update gfbrowser.cpp

--- gfbrowser.cpp
+++ gfbrowser.cpp
@@ -2,21 +2,26 @@
 #include <QGraphicsView>
 #include <QString>
 
-int main(int argc, char** argv) {
+int main(int argc, char *argv[])
+{
     QApplication app(argc, argv);
-    QString url;
-    QGraphicsView view;
-    // Read first application argument as url.
-    if (1 < app.arguments().count()) {url = app.arguments().at(1);}
-    else {url = "http://localhost";}
-    // Hide the cursor (it still exists).
-    QApplication::setOverrideCursor(Qt::BlankCursor);
-    // Disable scrollbars.
-    view.page()->mainFrame()->setScrollBarPolicy(Qt::Vertical, Qt::ScrollBarAlwaysOff);
-    view.page()->mainFrame()->setScrollBarPolicy(Qt::Horizontal, Qt::ScrollBarAlwaysOff);
-    // Set full screen.
-    view.showFullScreen();
-    // Open a url.
-    view.setUrl(QUrl(url));
+    const int width = 1900;
+    const int height = 1000;
+
+    QGraphicsScene scene;
+
+    QGraphicsView view(&scene);
+    view.setFrameShape(QFrame::NoFrame);
+    view.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+    view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
+
+    QGraphicsWebView webview;
+    webview.resize(width, height);
+    webview.load(QUrl("http://codepen.io/paulirish/pen/LsxyF"));
+
+    scene.addItem(&webview);
+    view.resize(width, height);
+    view.show();
+
     return app.exec();
 }
