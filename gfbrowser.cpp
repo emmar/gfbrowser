cf5dc65af96ca1e6981c4011cb282d54e27c4a9e
emmar <emma.ronney@gmail.com>
2015-06-15 19:56:36 +0000

    Update gfbrowser.cpp

--- gfbrowser.cpp
+++ gfbrowser.cpp
@@ -9,12 +9,13 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    QString url;
+    // Hide the cursor (it still exists).
+    QApplication::setOverrideCursor(Qt::BlankCursor);
     // Read first application argument as url.
+    QString url;
     if (1 < app.arguments().count()) {url = app.arguments().at(1);}
     else {url = "http://localhost";}
-    // Hide the cursor (it still exists).
-    //QApplication::setOverrideCursor(Qt::BlankCursor);
+    // Create the web content object. 
     QGraphicsScene scene;
     QGraphicsView view(&scene);
     QGraphicsWebView webView;
@@ -28,40 +29,11 @@ int main(int argc, char *argv[])
     // Disable scrollbars of view.
     view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
     view.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    // bugfix for QGraphicsWebView
+    // Bugfix for QGraphicsWebView not same as QWebView.
     webView.resize(app.desktop()->screenGeometry().size());
     // Disable scrollbars of webView.
     QWebFrame* frame = webView.page()->mainFrame();
     frame->setScrollBarPolicy(Qt::Vertical, Qt::ScrollBarAlwaysOff);
     frame->setScrollBarPolicy(Qt::Horizontal, Qt::ScrollBarAlwaysOff);
-
-    //QGraphicsScene scene;
-    //QGraphicsView view(&scene);
-/* QGraphicsView view;
-
-    // Disable scrollbars.
-    view.setFrameShape(QFrame::NoFrame);
-    view.setVerticalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
-    // Set full screen.
-    view.showFullScreen();
-    // reccommended - http://blog.qt.io/blog/2010/05/17/qtwebkit-now-accelerates-css-animations-3d-transforms/
-    view.setViewportUpdateMode(QGraphicsView::BoundingRectViewportUpdate);
-    view.setViewport(new QGLWidget);
-*/
-    //QGraphicsWebView webContent;
-
-    //webContent.load(QUrl(url));
- //QGraphicsView::fitInView(view.itemsBoundingRect(), Qt::KeepAspectRatio;
- //graphicsView.setResizesToContents(true);
-// graphicsView.fitInView();
-//scene.setSceneRect();
-
-  //  scene.addItem(&graphicsView);
-    //view.resize(width, height);
-
-    //view.show();
-    //webContent.showFullScreen();
-
     return app.exec();
 }
