46b70d79a7eafb6fd9393e8edc9c70ac3ba0ce87
emmar <emma.ronney@gmail.com>
2015-06-15 16:17:15 +0000

    Update gfbrowser.cpp

--- gfbrowser.cpp
+++ gfbrowser.cpp
@@ -1,31 +1,23 @@
 #include <QApplication>
-#include <QGraphicsScene>
-#include <QGraphicsView>
 #include <QGraphicsWebView>
+#include <QWebFrame>
 #include <QString>
 
 int main(int argc, char** argv) {
     QApplication app(argc, argv);
     QString url;
-    QGraphicsScene scene;
-    //QGraphicsView view(&scene);
-    QGraphicsWebView* webView  = new QGraphicsWebView();
+    QGraphicsWebView view;
     // Read first application argument as url.
     if (1 < app.arguments().count()) {url = app.arguments().at(1);}
     else {url = "http://localhost";}
-    // Open a url.
-    webView->load(QUrl(url));
     // Hide the cursor (it still exists).
     QApplication::setOverrideCursor(Qt::BlankCursor);
     // Disable scrollbars.
-    //webView.page()->mainFrame()->setScrollBarPolicy(Qt::Vertical, Qt::ScrollBarAlwaysOff);
-    //webView.page()->mainFrame()->setScrollBarPolicy(Qt::Horizontal, Qt::ScrollBarAlwaysOff);
+    view.page()->mainFrame()->setScrollBarPolicy(Qt::Vertical, Qt::ScrollBarAlwaysOff);
+    view.page()->mainFrame()->setScrollBarPolicy(Qt::Horizontal, Qt::ScrollBarAlwaysOff);
     // Set full screen.
-      QTransform transform;
-      transform.rotate(60,Qt::YAxis);
-      
-      webView->setTransform(transform);
-      webView->setPreferredSize(500,500);
-      //view.showFullScreen();  
+    view.showFullScreen();
+    // Open a url.
+    view.setUrl(QUrl(url));
     return app.exec();
 }
