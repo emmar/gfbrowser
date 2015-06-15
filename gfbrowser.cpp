188145009cc640fcff5560cb3d950045ea91186d
emmar <emma.ronney@gmail.com>
2015-06-15 16:57:26 +0000

    Update gfbrowser.cpp

--- gfbrowser.cpp
+++ gfbrowser.cpp
@@ -18,8 +18,8 @@ int main(int argc, char *argv[])
     view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
 
     QGraphicsWebView graphicsView;
-graphicsView()->setViewportUpdateMode(QGraphicsView::BoundingRectViewportUpdate);
-graphicsView()->setViewport(new QGLWidget);
+view->setViewportUpdateMode(QGraphicsView::BoundingRectViewportUpdate);
+view->setViewport(new QGLWidget);
     graphicsView.resize(width, height);
     graphicsView.load(QUrl("http://codepen.io/paulirish/pen/LsxyF"));
 
