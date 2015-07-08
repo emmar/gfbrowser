19993c9bf25d5696196b67e7f4b1549b507a850b
emmar <emma.ronney@gmail.com>
2015-07-08 23:29:09 +0000

    Configure view properties.

--- gfbrowser.cpp
+++ gfbrowser.cpp
@@ -7,6 +7,7 @@
 #ifdef DEV_INTERFACE_REQUIRES_MULTI_WINDOW
     #include <QWebInspector>
 #endif
+#include <QGLWidget>
 
 /*  Implementation that uses QGraphicsWebView to support hardware acceleration
  *  on mobile devices.
@@ -15,6 +16,38 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
+    
+    /*  Specifies whether images are automatically loaded in web pages. This is
+     *  enabled by default.
+     */
+    QWebSettings::globalSettings()->setAttribute(QWebSettings::AutoLoadImages, true);
+    /*  Enables or disables the running of JavaScript programs. This is enabled by
+     *  default.
+     */
+    QWebSettings::globalSettings()->setAttribute(QWebSettings::JavascriptEnabled, true);
+    /*  Private browsing prevents WebKit from recording visited pages in the
+     *  history and storing web page icons. This is disabled by default.
+     */
+    QWebSettings::globalSettings()->setAttribute(QWebSettings::PrivateBrowsingEnabled, true);
+    /*  This feature, when used in conjunction with QGraphicsWebView, accelerates
+     *  animations of web content. CSS animations of the transform and opacity
+     *  properties will be rendered by composing the cached content of the animated
+     *  elements. This is enabled by default.
+     */
+    QWebSettings::globalSettings()->setAttribute(QWebSettings::AcceleratedCompositingEnabled, true);
+    /*  This setting enables the tiled backing store feature for a
+     *  QGraphicsWebView. With the tiled backing store enabled, the web page
+     *  contents in and around the current visible area is speculatively cached
+     *  to bitmap tiles. The tiles are automatically kept in sync with the web page
+     *  as it changes. Enabling tiling can significantly speed up painting heavy
+     *  operations like scrolling. Enabling the feature increases memory
+     *  consumption. It does not work well with contents using CSS fixed
+     *  positioning (see also resizesToContents property).
+     *  tiledBackingStoreFrozen property allows application to temporarily freeze
+     *  the contents of the backing store. This is disabled by default.
+     */
+    QWebSettings::globalSettings()->setAttribute(QWebSettings::TiledBackingStoreEnabled, true);
+    
     // Hide the cursor (it still exists).
     QApplication::setOverrideCursor(Qt::BlankCursor);
     // Read first application argument as url.
@@ -38,11 +71,21 @@ int main(int argc, char *argv[])
     #endif
     // Add the webView to scene.
     scene.addItem(&webView);
-    // Reccommended - http://blog.qt.io/blog/2010/05/17/qtwebkit-now-accelerates-css-animations-3d-transforms
-    //view.setViewportUpdateMode(QGraphicsView::BoundingRectViewportUpdate);
-    //view.setViewport(new QGLWidget);
+    // Configure view properties.
+    view.setViewportUpdateMode(QGraphicsView::FullViewportUpdate);
+    view.setViewport(new QGLWidget(QGL::DirectRendering | QGL::DoubleBuffer));
+    view.setHorizontalScrollBarPolicy ( Qt::ScrollBarAlwaysOff );
+    view.setVerticalScrollBarPolicy ( Qt::ScrollBarAlwaysOff );
+    view.setWindowFlags(Qt::FramelessWindowHint);
+    view.showFullScreen();
+    #ifdef QT_BLOG_RECOMMENDS
+        // Reccommended - http://blog.qt.io/blog/2010/05/17/qtwebkit-now-accelerates-css-animations-3d-transforms
+        view.setViewportUpdateMode(QGraphicsView::BoundingRectViewportUpdate);
+        view.setViewport(new QGLWidget);
+    #endif
     // Load url.
     webView.load(QUrl(url));
+    view.setStyleSheet( "QGraphicsView { border-style: none; }" );
     view.showFullScreen();
     // Disable scrollbars of view.
     view.setHorizontalScrollBarPolicy(Qt::ScrollBarAlwaysOff);
